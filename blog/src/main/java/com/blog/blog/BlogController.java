1484745c602ad3fc5f3d62b39426e5f38d1c5f73
Namatirai <103169539+Namatirai@users.noreply.github.com>
2023-10-25 14:56:49 +0100

    edit and delete rest apis

--- blog/src/main/java/com/blog/blog/BlogController.java
+++ blog/src/main/java/com/blog/blog/BlogController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -36,4 +37,34 @@ public class BlogController {
         return "redirect:/blogs";
 
     }
+
+    @GetMapping("/deletepost/{id}")
+    public  String deleteBlog(@PathVariable Integer id){
+        blogRepository.deleteById(id);
+        return "redirect:/blogs";
+    }
+
+    @GetMapping("/editpost/{id}")
+        public String getEditForm(@PathVariable Integer id,Model model){
+        Blog blog = blogRepository.findById(id).orElse(null);
+
+        if (blog == null){
+            return "error";
+        }
+        model.addAttribute("blog",blog);
+        return "editblog";
+        }
+
+        @PostMapping("/editpost/{id}")
+    public  String editBlog(@PathVariable Integer id,@ModelAttribute Blog updatedblog){
+        Blog blog = blogRepository.findById(id).orElse(null);
+        if (blog == null){
+            return "error";
+        }
+        blog.setTitle(updatedblog.getTitle());
+        blog.setAuthor(updatedblog.getAuthor());
+        blog.setContent(updatedblog.getContent());
+        blogRepository.save(blog);
+        return "redirect:/blogs";
+        }
 }
